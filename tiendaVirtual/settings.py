6f3dba3f93198b41d85565b560e8ed7d34528765
maikoledu <edulozano.30@gmail.com>
2014-01-22 16:28:33 -0500

    configuracion

--- tiendaVirtual/settings.py
+++ tiendaVirtual/settings.py
@@ -1,7 +1,12 @@
+#encoding:utf-8
 # Django settings for tiendaVirtual project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -11,8 +16,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'tienda.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -29,11 +34,11 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Lima'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-PE'
 
 SITE_ID = 1
 
@@ -107,24 +112,26 @@ ROOT_URLCONF = 'tiendaVirtual.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'tiendaVirtual.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
-INSTALLED_APPS = (
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+
+DJANGO_APPS= (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
 )
+
+LOCAL_APPS = (
+    'app.principal',
+)
+
+INSTALLED_APPS = LOCAL_APPS + DJANGO_APPS
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
